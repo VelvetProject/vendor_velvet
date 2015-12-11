22973356ace3d3580e4127108531e54bb588a986
George G <kreach3r@users.noreply.github.com>
2015-12-11 21:40:00 +0200

    Dynamically load new makefiles and vendorsetups
    This is an end to hardcoding makefiles and vendorsetups. Partially taken from CM: https://github.com/CyanogenMod/android_vendor_cm/blob/cm-12.1/vendorsetup.sh
    
    * AndroidProducts: finds every file starting with "velvet_" inside vendor/velvet/products and loads it into the build sequence
    * vendorsetup: finds same files and replaces .mk with $variant build variable (defaults to "user")

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,21 +1,15 @@
-# Copyright (C) 2016 The Pure Nexus Project
-#               2016 The Velvet Project
+#     Copyright (C) 2015 KreAch3R
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#          http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
 
-ifeq (velvet_angler,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/velvet_angler.mk
-endif
-ifeq (velvet_bullhead,$(TARGET_PRODUCT))
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/velvet_bullhead.mk
-endif
+PRODUCT_MAKEFILES := $(shell find $(LOCAL_DIR) -type f | grep velvet_)
