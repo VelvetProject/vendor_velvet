6d719ceb7468c154d55c5d29bd90b8b680c90517
Ivan Iskandar <iiiiskandar14@gmail.com>
2018-02-08 08:48:58 +0700

    Add bullhead svelte

---
+++ products/velvet_bullhead_svlt.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2016 The Pure Nexus Project
+#               2016 The Velvet Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include Velvet telephony configuration
+include vendor/velvet/configs/velvet_phone.mk
+
+# Inherit AOSP device configuration for bullhead
+$(call inherit-product, device/lge/bullhead/aosp_bullhead_svelte.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := velvet_bullhead_svlt
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := bullhead
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM3.171019.013/4499252:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM3.171019.013 4499252 release-keys"
